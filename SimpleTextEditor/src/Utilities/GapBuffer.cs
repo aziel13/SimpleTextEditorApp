92d9f76628ba9c0d29552a9c78fcb6c38840b97a
tud50806 <tud50806@temple.edu>
2026-02-27 02:35:10 -0500

    Implemented gap buffer init,  growBuffer, and insert.
    todo left and right remove charecter

--- SimpleTextEditor/src/Utilities/GapBuffer.cs
+++ SimpleTextEditor/src/Utilities/GapBuffer.cs
@@ -1,6 +1,64 @@
 ﻿namespace SimpleTextEditor.Utilities;
 
-public class GapBuffer
+public class GapBuffer<T>
 {
+    private int _capacity;
+    private T[] _buffer;
+    private int _gapStart;
+    private int _gapEnd;
     
+    public T[] Buffer => _buffer;
+    
+    public int Capacity => _capacity;
+    public int GapStart => _gapStart;
+    public int GapEnd => _gapEnd;
+    
+    public GapBuffer(int capacity) {
+        
+        _capacity = capacity;
+        _buffer = new T[_capacity];
+        _gapStart = 0;
+        _gapEnd = _capacity - 1;
+        
+    }
+
+    public void Insert(T input)
+    {
+        if (_gapStart == _gapEnd)
+        {
+            GrowBuffer();
+        }
+
+        _buffer[_gapStart] = input;
+        
+        _gapStart++;
+    }
+
+    public void GrowBuffer() {
+        
+        
+        int new_capacity =  _capacity * 2;
+        if (new_capacity > 0)
+        {
+
+            T[] newBuffer = new T[new_capacity];
+            
+            /*
+            for (int i = 0; i < GapStart; i++)
+            {
+                newBuffer[i] = _buffer[i];
+            }
+            for (int i = GapEnd+1; i < _capacity; i++)
+            {
+                newBuffer[i] = _buffer[i];
+            }*/
+
+
+            _capacity = new_capacity;
+            _buffer = newBuffer;
+        }
+
+    }
+
+
 }

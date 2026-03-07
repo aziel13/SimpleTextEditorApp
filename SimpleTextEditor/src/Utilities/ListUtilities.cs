57342d472ffa747bced310c2ed76348dece5e51a
aziel13 <avatarofwill13@gmail.com>
2026-03-07 09:33:21 -0500

    Created several insert tests to cover edge cases. Wrote the code to make failing tests go green without causing other tests to fail. Solution works sofar but probably needs refactoring.

---
+++ SimpleTextEditor/src/Utilities/ListUtilities.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleTextEditor.Utilities;
+
+public static class ListUtilities
+{
+    
+    public static List<T> ListDeepCopy<T>(List<T> deepCopy)
+    {
+        List<T> newList = new List<T>();
+
+        for (int i = 0; i < deepCopy.Count; i++) {
+            
+            newList.Add(deepCopy[i]);
+            
+        }
+
+        return newList;
+    }
+
+}
--- SimpleTextEditor/src/Utilities/PieceTable.cs
+++ SimpleTextEditor/src/Utilities/PieceTable.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleTextEditor.Utilities;
+﻿using System.Diagnostics;
+
+namespace SimpleTextEditor.Utilities;
 
 public enum PieceEnum
 {
@@ -86,12 +88,112 @@ public class PieceTableDataStructure : IPieceTable
     
     public void insertText(int index, string text)
     {
+        string originalBuffer = _pieceTable.originalBuffer;
+        
+        List<Piece> piecesTablePiecesListDeepCopy =  ListUtilities.ListDeepCopy(_pieceTable.pieces);
         
         int newStart = _pieceTable.addBuffer.Length;
         
+        int totalTextLength = _pieceTable.originalBuffer.Length + _pieceTable.addBuffer.Length; 
+        
+        Piece newPiece = new Piece(newStart, text.Length, PieceEnum.ADD);
+        
+        if (index == 0)
+        {
+            piecesTablePiecesListDeepCopy.Insert(0, newPiece);
+            
+        } else if (index >= totalTextLength) {
+            
+            piecesTablePiecesListDeepCopy.Add(newPiece);
+            
+        }
+        else
+        {
+
+            int stringLengthSofar = 0;
+            
+            for (int i = 0; i < _pieceTable.pieces.Count ; i++)
+            {
+
+                Piece thisPiece = _pieceTable.pieces[i];
+                
+                stringLengthSofar += thisPiece.Length;
+                if (index == stringLengthSofar)
+                {
+                    
+                    piecesTablePiecesListDeepCopy.Insert(i+1, newPiece);
+                    
+                    break;
+                }
+                else if (index < stringLengthSofar)
+                {
+                    
+                    int beforeLength;
+
+                    int beforeIndex = _pieceTable.pieces[i].Start;
+                    int afterLength;
+                    int afterIndex;
+                    
+                    if ((_pieceTable.pieces[i].Source == PieceEnum.ORIGINAL && _pieceTable.originalBuffer.Length > index)) 
+                    {
+                        beforeLength = index;
+                        afterLength = _pieceTable.pieces[i].Length - beforeLength;
+                        afterIndex = beforeLength;
+                    }
+                    else
+                    {
+                        if (_pieceTable.pieces[i].Source == PieceEnum.ORIGINAL)
+                        {
+                            beforeLength = Math.Abs(_pieceTable.originalBuffer.Length  - text.Length);
+                            afterLength =  Math.Abs( _pieceTable.pieces[i].Length - beforeLength);
+                            afterIndex = beforeLength;
+
+                        }
+                        else
+                        {
+                            int relativeIndex = stringLengthSofar - (index);
+                            int indexAfterNewAddition = relativeIndex + text.Length;
+                            
+                            Console.WriteLine($"relativeIndex: {relativeIndex} + text.length = index at end of new add {indexAfterNewAddition} after length { thisPiece.Length - relativeIndex}"); 
+
+                           
+                            // Console.WriteLine($"stringLengthSofar-index: {stringLengthSofar-index-1}");
+                            
+                            Console.WriteLine($"index: {index}");
+
+                            
+                            beforeLength = relativeIndex; 
+                            afterLength =  thisPiece.Length - relativeIndex;
+                            afterIndex = beforeLength;
+                            
+                        }
+                    } 
+                    
+                    Console.WriteLine($"beforeLength: {beforeLength}");
+                    
+                    Console.WriteLine($"afterLength: {afterLength}");
+                    Console.WriteLine($"_pieceTable.pieces[i].Length: {_pieceTable.pieces[i].Length}");
+                    
+                    
+                    Piece beforePiece = new Piece(beforeIndex , beforeLength, _pieceTable.pieces[i].Source);;
+                    Piece afterPiece = new Piece(afterIndex, afterLength, _pieceTable.pieces[i].Source  );
+
+                    piecesTablePiecesListDeepCopy.RemoveAt(i);
+                    piecesTablePiecesListDeepCopy.Insert(i, beforePiece);
+                    piecesTablePiecesListDeepCopy.Insert(i+1, newPiece);
+                    piecesTablePiecesListDeepCopy.Insert(i+2, afterPiece);
+                    
+                    break;
+                }
+            
+            }
+            
+        }
+        
+
         _pieceTable.addBuffer += text;
 
-        _pieceTable.pieces.Add(new Piece(newStart,text.Length,PieceEnum.ADD));
+        _pieceTable.pieces = piecesTablePiecesListDeepCopy;
 
     }
 
@@ -104,19 +206,20 @@ public class PieceTableDataStructure : IPieceTable
     {
          string result = string.Empty;
          
+         List<string> documentConstructionList = new List<string>();
+         
         //$"{_pieceTable.originalBuffer}{_pieceTable.addBuffer}"
          for (int i = 0; i < _pieceTable.pieces.Count; i++) {
 
              Piece piece = _pieceTable.pieces[i];
              
-             
              if (piece.Source == PieceEnum.ORIGINAL)
              {
-                 result += _pieceTable.originalBuffer.Substring(piece.Start, piece.Start + piece.Length); 
+                 result += _pieceTable.originalBuffer.Substring(piece.Start,  piece.Length); 
              }
              else
              {
-                 result += _pieceTable.addBuffer.Substring(piece.Start, piece.Start + piece.Length); 
+                 result += _pieceTable.addBuffer.Substring(piece.Start,  piece.Length); 
              }
 
          }
